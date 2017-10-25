4e163b58dbed834871b807845b4ab15b418b8e20
Carlo Moro <cnmoro@gmail.com>
2017-10-24 23:43:46 -0200

    Criadas classes base

---
+++ src/main/java/calendarioacademico/servicos/Controle.java
@@ -0,0 +1,20 @@
+package calendarioacademico.servicos;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Controle {
+
+	public static void main(String[] args) {
+
+		java.awt.EventQueue.invokeLater(() -> {
+                    try {
+                        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                    } catch (UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException | ClassNotFoundException ex) {
+                        ex.printStackTrace();
+                    }
+                    new InterfacePrincipal().setVisible(true);
+                });  
+
+	}
+}
